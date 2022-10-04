a6fc2b9ca93e477560438573abf923208d73a7d5
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-10-04 11:09:17 -0300

    niveis de registro

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,10 +9,14 @@ public class Example {
 	static Logger log = Logger.getLogger(Example.class.getName());
 
 	public static void main(String[] args) throws IOException, SQLException {
-		
-		log.debug("Hello this is a debug message");
-		log.info("Hello this is an info message");
-		
+
+		log.trace("Trace Message!");
+		log.debug("Debug Message!");
+		log.info("Info Message!");
+		log.warn("Warn Message!");
+		log.error("Error Message!");
+		log.fatal("Fatal Message!");
+
 	}
-	
+
 }
